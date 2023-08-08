1c68c192381beb609c24c57757265237ffcd2736
Daniel-Iosif Trubacs (dit1u20) <dit1u20@soton.ac.uk>
2023-08-08 09:26:36 +0100

    Created module for communication between the local machine and the brushed motors

---
+++ coms/thorlabs_brushed_motors_kinesis.py
@@ -0,0 +1,11 @@
+"""
+Created by Daniel-Iosif Trubacs for the UoS QLM group on 8 August 2023. The
+purpose of this module is to create a high level Client class for the
+ThorLabs Kinesis Brushed Motors that can handle communication between the
+external hardware and the local machine.
+
+See the end of the file for a code example.
+
+
+Last updated by Daniel-Iosif Trubacs on 8 August 2023.
+"""
